6629dbc927822478c76369f94729d3247a652aa1
Bronagh Colton <61869605+Bocolo@users.noreply.github.com>
2022-04-13 03:07:35 +0100

    updating test

--- SubmitCanvasManagerTests.cs
+++ SubmitCanvasManagerTests.cs
@@ -10,6 +10,9 @@ using UnityEngine.TestTools;
 
 public class SubmitCanvasManagerTests
 {
+    /// <summary>
+    /// do somthing to get all the inputs and set them
+    /// </summary>
     private Menu menu;
     private GameObject manager;
     private GameObject submitManager;
@@ -31,10 +34,11 @@ public class SubmitCanvasManagerTests
         yield return null;
 
         submitCanvasManager = submitManager.GetComponent<SubmitCanvasManager>();
+        //do all the get component and check those values
 
     }
- /*   [UnityTest]
-    public IEnumerator SubmitCanvasManager_ResetFields()
+    [UnityTest]
+    public IEnumerator SubmitCanvasManager_CompleteSubmission()
     {
         SaveData.Instance.SaveUserProfile(new User
         {
@@ -42,7 +46,7 @@ public class SubmitCanvasManagerTests
             Company = "Test Company"
         });
         yield return null;
-        submitCanvasManager.OnSubmitResetFields();
+        submitCanvasManager.CompleteSubmission();
         yield return null;
         Assert.AreEqual("Test Name",submitCanvasManager.Name.text);
         Assert.AreEqual("Test Company", submitCanvasManager.Company.text);
@@ -54,11 +58,39 @@ public class SubmitCanvasManagerTests
         Assert.AreEqual(0, submitCanvasManager.DayDrop.value);
         Assert.AreEqual(0, submitCanvasManager.MonthDrop.value);
         Assert.AreEqual(0, submitCanvasManager.YearDrop.value);
-    }*/
+        Assert.IsTrue(submitCanvasManager.SubmissionPopUp.isActiveAndEnabled);
+    }
+    [UnityTest]
+    public IEnumerator SubmitCanvasManager_CompleteStore()
+    {
+        SaveData.Instance.SaveUserProfile(new User
+        {
+            Name = "Test Name",
+            Company = "Test Company"
+        });
+        yield return null;
+        submitCanvasManager.CompleteStore();
+        yield return null;
+        Assert.AreEqual("Test Name", submitCanvasManager.Name.text);
+        Assert.AreEqual("Test Company", submitCanvasManager.Company.text);
+        Assert.AreEqual("", submitCanvasManager.Comments.text);
+        Assert.AreEqual(0, submitCanvasManager.Species.value);
+        Assert.AreEqual(0, submitCanvasManager.IceRectangle.value);
+        Assert.AreEqual(0, submitCanvasManager.SampleLocationName.value);
+        Assert.AreEqual(0, submitCanvasManager.ProductionWk.value);
+        Assert.AreEqual(0, submitCanvasManager.DayDrop.value);
+        Assert.AreEqual(0, submitCanvasManager.MonthDrop.value);
+        Assert.AreEqual(0, submitCanvasManager.YearDrop.value);
+        Assert.IsTrue(submitCanvasManager.SubmissionPopUp.isActiveAndEnabled);
+    }
     [UnityTest]
     public IEnumerator SubmitCanvasManager_SwitchCanvas()
     {
-        GameObject smallCanvas = submitCanvasManager.GetSmallCanvas();//GameObject.Find("/Canvas_SMALL");
+
+        ///gotta do get for compoenets
+        ///
+
+       /* GameObject smallCanvas = submitCanvasManager.GetSmallCanvas();//GameObject.Find("/Canvas_SMALL");
         GameObject largeCanvas = submitCanvasManager.GetLargeCanvas();//GameObject.Find("/Canvas_LARGE");
         Assert.False(smallCanvas.activeInHierarchy);
         Assert.True(largeCanvas.activeInHierarchy);
@@ -74,7 +106,7 @@ public class SubmitCanvasManagerTests
         Assert.False(largeCanvas.activeInHierarchy);
         Assert.AreEqual("Test Name", submitCanvasManager.Name.text);
         Assert.AreEqual("Test Company", submitCanvasManager.Company.text);
-      
+      */
     }
    /* [Test]//any need for this when edit test cover it
     public void PopUp_Active()
